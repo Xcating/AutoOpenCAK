e95c91a420841a5088c078617bc64bbab5ab77bd
micah <1569124799@qq.com>
2023-05-06 01:48:19 +0800

    添加获取文件路径方法  规划配置文件

--- OpenCorepiAndBypass/src/Program.cs
+++ OpenCorepiAndBypass/src/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace OpenCorepiAndBypass
 {
@@ -9,14 +11,15 @@ namespace OpenCorepiAndBypass
 
     class Program
     {
-
-        static void changeFileName(string oldName,string newName)
+        //改变文件名称
+        static void ChangeFileName(string oldName, string newName)
         {
             File.Delete(newName); // 删除已存在的文件
             File.Move(oldName, newName); // 将旧文件名改为新文件名
         }
 
-        static void openFile(string fileName)
+        //打开文件
+        static void OpenFile(string fileName)
         {
             Process process = new Process();
             // 设置要启动的文件名，包括完整路径
@@ -24,11 +27,39 @@ namespace OpenCorepiAndBypass
             // 启动Process对象
             process.Start();
         }
+
+        //获取文件路径
+        static string GetFilePath(string title,string filter)
+        {
+            string filePath = "";
+            Thread t = new Thread((ThreadStart)(() =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+                openFileDialog.Filter = filter;
+                openFileDialog.Title = title;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            ));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+            return filePath;
+        }
+
+
+
         static void Main(string[] args)
         {
 
-            Program.openFile(@"C:\Users\Administrator\Desktop\genshinhack\GenshinAccount.exe");
+            //TODO
+            //判断是否存在配置文件
+            //判断是否开启账号切换器/3dm/注入器/bypass/同时启动的辅助
 
+            Program.OpenFile(GetFilePath("选择账号切换器", "GenshinAccount.exe|GenshinAccount.exe"));
             System.Console.WriteLine("打开账户切换文件");
 
 
@@ -36,23 +67,24 @@ namespace OpenCorepiAndBypass
             Thread.Sleep(5000);
 
 
-            Program.openFile(@"C:\Users\Administrator\Desktop\genshinhack\3dmigoto\play\3DMigoto Loader.exe");
-
+            Program.OpenFile(@"C:\Users\Administrator\Desktop\genshinhack\3dmigoto\play\3DMigoto Loader.exe");
             System.Console.WriteLine("打开3dm");
+
+
             // 暂停1秒
             Thread.Sleep(1000);
 
 
             //bypass
             System.Console.WriteLine("bypass");
-            Program.changeFileName(@"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys", @"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys.bak");
-            Program.changeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll", @"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll.bak");
-            Program.changeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys", @"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys.bak");
+            Program.ChangeFileName(@"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys", @"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys.bak");
+            Program.ChangeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll", @"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll.bak");
+            Program.ChangeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys", @"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys.bak");
             // 暂停1秒
             Thread.Sleep(1000);
             //打开注入器
             System.Console.WriteLine("打开注入器");
-            Program.openFile(@"C:\Users\Administrator\Desktop\genshinhack\injector.exe");
+            Program.OpenFile(@"C:\Users\Administrator\Desktop\genshinhack\injector.exe");
 
             System.Console.WriteLine("暂停15秒");
             // 暂停20秒
@@ -60,16 +92,16 @@ namespace OpenCorepiAndBypass
 
             //取消bypass
             System.Console.WriteLine("取消bypass");
-            Program.changeFileName(@"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys.bak", @"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys");
-            Program.changeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll.bak", @"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll");
-            Program.changeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys.bak", @"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys");
+            Program.ChangeFileName(@"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys.bak", @"G:\Genshin Impact\Genshin Impact Game\HoYoKProtect.sys");
+            Program.ChangeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll.bak", @"G:\Genshin Impact\Genshin Impact Game\mhypbase.dll");
+            Program.ChangeFileName(@"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys.bak", @"G:\Genshin Impact\Genshin Impact Game\mhyprot3.Sys");
 
             // 暂停1秒
             Thread.Sleep(1000);
 
             //打开cb
             System.Console.WriteLine("打开cb");
-            Program.openFile(@"C:\Users\Administrator\Desktop\genshinhack\Cotton Buds 3.6 - Stable.CETRAINER");
+            Program.OpenFile(@"C:\Users\Administrator\Desktop\genshinhack\Cotton Buds 3.6 - Stable.CETRAINER");
 
 
             System.Console.WriteLine("程序完成,回车退出");
